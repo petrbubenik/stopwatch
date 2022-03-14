f0536087334691974b6d3c272250d3eb7e5cdbd3
Petr Bubenik <petrbubenik@gmail.com>
2022-03-14 18:47:31 +0000

    code optimization; lap times standartized (MM:ss:mm)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 // Goals:
-// - dodelat mezicas - casy se ukazuji bez pocatecnich nul
 // - dat stop a reset do stejneho buttonu
 // - presnejsi mereni casu
 // - double click on start does not allow to stop/reset
 // - time difference between laps
 
 
-let minute = 0;
-let second = 0;
-let msecond = 0;
+let minute = "00";
+let second = "00";
+let msecond = "00";
 let interval;
 let lapNow;
 
@@ -20,22 +19,19 @@ Array.from(buttons).forEach(element => element.addEventListener('click', pushBut
 
 function pushButton(event){
     
-    // If Start button - start the watch
+    // If Start button pushed - start the watch:
     if (event.target.classList.contains('start')){
         interval = setInterval(startTimer);
-
-    // If Lap button - record the time
     } else if(event.target.classList.contains('lap')){
-        lapNow = `<div class="lap">${minute} : ${second} : ${msecond}</div>`;
-        lapRecord.innerHTML += lapNow; // document.querySelector('#lapRecord').innerHTML += lapNow;
-
-    
-    // If Stop button - stop the watch and keep the time
+        lapTime()
+            
+    // If Stop button pushed - stop the watch and log the time:
     } else if(event.target.classList.contains('stop')){
         clearInterval(interval);
+        lapTime()
     
     
-    // If Reset button - reset the watch to zero
+    // If Reset button pushed - reset the watch to zero and remove lap times:
     } else if(event.target.classList.contains('reset')){
         clearInterval(interval);
         minute = "00";
@@ -48,30 +44,41 @@ function pushButton(event){
     }
 }
 
+// If Lap button pushed - record the time
+function lapTime(){
+    lapNow = `<div class="lap">${minute} : ${second} : ${msecond}</div>`;
+    lapRecord.innerHTML += lapNow;
+}
+
 function startTimer(){
     msecond++;
- 	if(msecond < 9){
- 		document.querySelector('.msec').innerHTML = "0" + msecond;
- 	}
- 	if(msecond > 9){
-	document.querySelector('.msec').innerHTML = msecond;
- 	}
- 	if(msecond > 99){
+ 	if(msecond < 10){
+        msecond = "0" + msecond;
+ 		document.querySelector('.msec').innerHTML = msecond;
+ 	}else if(msecond >= 10 && msecond < 100){
+	    document.querySelector('.msec').innerHTML = msecond;
+ 	}else if(msecond = 100){
  		second++;
- 		document.querySelector('.sec').innerHTML = "0" +second;
+        second = "0" + second;
+        document.querySelector('.sec').innerHTML = second;
  		msecond =0;
  		document.querySelector('.msec').innerHTML = "0" + 0; 	
- 	}
- 	if (second > 9){
+    }
+ 	
+    if (second > 9){
+        second = second.slice(-2);
  		document.querySelector('.sec').innerHTML = second;
  	}
+    
     if (second > 59){
         minute++;
-        document.querySelector('.min').innerHTML = "0" + minute;
+        minute = "0" + minute;
+        document.querySelector('.min').innerHTML = minute;
         second = 0;
         document.querySelector('.sec').innerHTML = "0" + 0;
     }
     if (minute > 9){
+        minute = minute.slice(-2);
         document.querySelector('.min').innerHTML = minute;
     }
 
